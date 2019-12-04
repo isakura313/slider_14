cb2e6e26338aed148b06f2e809350fd914d0a3e9
Paul <isakura313@gmail.com>
2019-12-04 20:29:33 +0300

    first

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 let slider = $(".slider");
-let sliderItems = slider.find(".slider-item");
+let sliderLenta = slider.find('.slider-lenta');
+let sliderItems = slider.find(".slider-lenta-item.real");
+sliderLenta.css('width', (sliderItems.length+2) * 100 + "%");
 
 // MVC
 //Model
@@ -8,17 +10,30 @@ let showIndex = 0;
 
 
 slider.find('.slider-arrow-left').click(function(){
-
+    if(sliderLenta.hasClass("animated")){
+        return;
+    }
+    sliderLenta.addClass("animated");
     if( showIndex <= 0){
-        sliderItems.eq(showIndex).fadeOut();
-        showIndex = sliderItems.length - 1;
-        sliderItems.eq(showIndex).css('display', 'flex');
-    } else{
-        sliderItems.eq(showIndex).fadeOut();
-        sliderItems.eq(showIndex-1).css('display', 'flex');
         showIndex--;
+
+        sliderLenta.animate({
+            left: -100 * (showIndex+1)+"%" // тут был fadeOut
+        }, 600, function(){
+            showIndex = sliderItems.length - 1;
+            sliderLenta.css('left', -100 * (showIndex+1)+"%");
+            sliderLenta.removeClass("animated");
+        });
+    } else{
+        showIndex--;
+        sliderLenta.animate({
+            left: -100 * (showIndex + 1)+"%" // тут был fadeOut
+        }, 600, function(){
+            sliderLenta.removeClass("animated");
+        }); 
     }
 })
+
 slider.find('.slider-arrow-right').click(function(){
     if(showIndex >= sliderItems.length - 1){
         sliderItems.eq(showIndex).fadeOut();
