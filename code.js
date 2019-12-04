e6602fccd16943787fcd9c9237f9102393a4d4b5
Paul <isakura313@gmail.com>
2019-12-04 19:21:44 +0300

    adaptive

--- code.js
+++ code.js
@@ -11,12 +11,22 @@ slider.find('.slider-arrow-left').click(function(){
 
     if( showIndex <= 0){
         sliderItems.eq(showIndex).fadeOut();
-        sliderItems[showIndex]
         showIndex = sliderItems.length - 1;
         sliderItems.eq(showIndex).css('display', 'flex');
     } else{
         sliderItems.eq(showIndex).fadeOut();
         sliderItems.eq(showIndex-1).css('display', 'flex');
         showIndex--;
+    }
+})
+slider.find('.slider-arrow-right').click(function(){
+    if(showIndex >= sliderItems.length - 1){
+        sliderItems.eq(showIndex).fadeOut();
+        showIndex = 0;
+        sliderItems.eq(showIndex).css('display', 'flex');
+    } else{
+        sliderItems.eq(showIndex).fadeOut();
+        sliderItems.eq(showIndex+1).css('display', 'flex');
+        showIndex++;
     }
 })
